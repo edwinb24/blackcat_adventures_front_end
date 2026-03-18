074b4598a04e2cd025e9c62bb2a1d1a7bf117a97
Edwin <edwin_b24@hotmail.com>
2026-03-18 17:06:09 -0500

    Correcting Icon Paths

--- src/app/utils/icons/twitchIcon.tsx
+++ src/app/utils/icons/twitchIcon.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 
 const TwitchIcon: React.FC<SvgIconProps> = props => (
     <SvgIcon {...props} viewBox='0 0 24 24'>
-        <path d='M4 2L2 6v14h6v4h4l4-4h4l4-4V2H4zm16 12l-2 2h-4l-2 2v-2H6V4h14v10zM9 7h2v5H9V7zm4 0h2v5h-2V7z' />
+        <path d='M3.857 0 1 2.857v10.286h3.429V16l2.857-2.857H9.57L14.714 8V0zm9.714 7.429-2.285 2.285H9l-2 2v-2H4.429V1.143h9.142z' />
+        <path d='M11.857 3.143h-1.143V6.57h1.143zm-3.143 0H7.571V6.57h1.143z' />
     </SvgIcon>
 )
 
